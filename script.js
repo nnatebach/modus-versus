1d3511cb6fcaf94884c257a509e71c9359a63365
Nhan Bach <minhnhan.fastcodingvn@gmail.com>
2021-01-26 19:42:50 +0700

    finished slick and fixed dropdown list hovering

--- script.js
+++ script.js
@@ -15,4 +15,20 @@ $(document).ready(function(){
       }
     ]
   })
+  $('.client-list').slick({
+    infinite: true,
+    speed: 500,
+    arrows: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: false,
+          slidesToShow: 3
+        }
+      }
+    ]
+  })
 })
